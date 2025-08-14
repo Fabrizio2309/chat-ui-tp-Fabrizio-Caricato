652f721ee3538c742669c60c15ff42b7ab877cb6
Fabrizio <fabriziocaricato1@gmail.com>
2025-08-14 01:45:02 -0300

    selectores HTML creados y funcion creada

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
+const inputMensaje = document.querySelector(".contenedor-envio-mensaje input")
+const contenedorMensajes = document.querySelector(".contenedor-mensajes")
+const botonEnvio = document.querySelector(".contenedor-envio-mensaje button")
 
+botonEnvio.addEventListener("click", envioDeMensaje())
 
+function envioDeMensaje() {
+}
